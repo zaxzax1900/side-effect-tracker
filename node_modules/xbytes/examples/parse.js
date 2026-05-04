3c9b2048fe451890eb03d2cd80031fba574245c7
unknown <zaxzax1996@163.com>
2026-05-04 18:42:49 +0800

    Clean: remove node_modules and npm files

--- node_modules/xbytes/examples/parse.js
+++
@@ -1,6 +0,0 @@
-const xbytes = require('../lib');
-
-const input = process.argv.slice(2).join(' ');
-const parsed = input.replace(xbytes.globalByteFilter, xbytes.createSizeParser({bits: false, iec: false}));
-
-console.log(parsed);
